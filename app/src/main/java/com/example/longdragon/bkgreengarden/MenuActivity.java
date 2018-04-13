f889a8e12ef00be6b2d07f923dc59f3ebeb15ddd
01101100 <long.dragon96@gmail.com>
2018-04-13 17:54:47 +0700

    show tree information

--- app/src/main/java/com/example/longdragon/bkgreengarden/MenuActivity.java
+++ app/src/main/java/com/example/longdragon/bkgreengarden/MenuActivity.java
@@ -1,8 +1,10 @@
 package com.example.longdragon.bkgreengarden;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +14,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 public class MenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +43,20 @@ public class MenuActivity extends AppCompatActivity
 //        toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        layout=(RelativeLayout)(findViewById(R.id.app_bar_menu).findViewById(R.id.include).findViewById(R.id.info));
+        View.OnClickListener click= new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.print(R.id.info);
+                Log.d("id", "onClick: +"+v.getId());
+                if(v.getId()!=R.id.info){
+                    layout.removeAllViews();
+                }
+            }
+        };
+        ((findViewById(R.id.app_bar_menu).findViewById(R.id.include))).setOnClickListener(click);
+        layout.setOnClickListener(click);
+
         navigationView.setNavigationItemSelectedListener(this);
     }
 
@@ -81,8 +99,9 @@ public class MenuActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            layout.addView(getLayoutInflater().inflate(R.layout.tree_information,layout,false));
         } else if (id == R.id.nav_gallery) {
+
 
         } else if (id == R.id.nav_slideshow) {
 
